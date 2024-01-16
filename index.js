60ca1a8c5c5ba0f272373874fb34c22ba62c52dd
Cyrillus <chenetcyril@free.fr>
2024-01-16 19:11:05 +0100

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import app from './app/index.js';
+import cors from 'cors';
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 if (process.env.NODE_ENV !== 'production') {
   import('dotenv/config');
 }
+
+app.use(cors());
 
 const port = process.env.PORT || 3001;
 
