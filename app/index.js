4c1ab900a09f3dc0d2f020881f0f4b43472907e0
Cyrillus <chenetcyril@free.fr>
2024-03-25 10:42:41 +0100

    Modif: add example cors secure

--- app/index.js
+++ app/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-
-import router from './routers/index.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerOptions from './swagger-doc/swagger.js';
 
+import router from './routers/index.js';
+
 const app = express();
+
+// Allow only http://evalie.fr/ to access
+// const corsOptions = {
+//     origin: 'http://evalie.fr',
+//     optionsSuccessStatus: 200
+//   };
+// app.use(cors(corsOptions));
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
